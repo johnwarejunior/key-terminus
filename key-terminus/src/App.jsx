0bc427e71c6f817fce8d4d3cddd5a95205fa0141
johnwarejunior <john@johnwarejunior.com>
2023-06-21 14:01:53 -0700

    Project setup with file clean-up.

---
+++ key-terminus/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Box } from '@chakra-ui/react'
+
+
+const App = () => {
+  return (
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <Container maxW='3xl' centerContent>
+      <h1>My App</h1>
+      </Container>
+      
+    </Box>
+  )
+}
+
+export default App
