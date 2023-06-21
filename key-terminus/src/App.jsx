adc008a32d9c884ca2d436b5d7a6575e9b47ecba
johnwarejunior <john@johnwarejunior.com>
2023-06-21 15:37:07 -0700

    Add API and change ENV

--- key-terminus/src/App.jsx
+++ key-terminus/src/App.jsx
@@ -6,11 +6,16 @@ import TextInput from './components/TextInput'
 
 
 const App = () => {
+
+  const extractKeywords = (text) => {
+    console.log(text);
+  };
+
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
       <Header />
-      <TextInput />
+      <TextInput extractKeywords={extractKeywords}/>
       <Footer />
       </Container>
       
