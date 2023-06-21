36ad848aacf758b18099f75fb597247c16580309
johnwarejunior <john@johnwarejunior.com>
2023-06-21 14:41:42 -0700

    Build footer component.

--- key-terminus/src/App.jsx
+++ key-terminus/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Container, Box } from '@chakra-ui/react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
       <Container maxW='3xl' centerContent>
       <Header />
+      <Footer />
       </Container>
       
     </Box>
